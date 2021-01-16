262cf58f3b80310835bec07044a510e0ccc114d6
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-01-15 20:25:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,62 +1,63 @@
 const express = require("express");
 const app = express();
-const path = require('path'); //use a node.js self module
-const Campground = require('./models/campground') //get exported model from using mongoose
-const methodOverride = require('method-override'); //a node.js module, override the req.method property
+const path = require('path');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
-const mongoose = require('mongoose'); //connect to local databasee
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }); 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-//sample code from mongoose to connect to mongoose databse
-const db = mongoose.connection; 
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log('database connected!')
 });
 
-app.set('view engine', 'ejs'); //set view to EJS
-app.set('views', path.join(__dirname, '/views')); //join all folders in 'views' folder
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.urlencoded({ extended: true })) //parse json and urlencoded 
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds }) //pass a Campground object to a view file(views/campgrounds/index.ejs)
+    res.render('campgrounds/index', { campgrounds })
 })
 
 app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new') //display webpage according to 'views/campgrounds/new.ejs'
+    res.render('campgrounds/new')
 })
 
 app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground); //in 'views/campgrounds/new.ejs', <input type="text" id="title" name="campground[title]">
+    const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`) //Mongoose default value: _id - ObjectId, id - string
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
-app.get('/campgrounds/:id', async (req, res) => { //Express route path defined: Request URL- http://localhost:3000/campgrounds/1234567
-    const campground = await Campground.findById(req.params.id) //req.params.id - {"id": "1234567"}, get the value in that position
-    res.render('campgrounds/show', { campground }) 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
 })
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id) 
+    const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground })
 })
 
 app.put('/campgrounds/:id', async (req, res) => {
-    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground }); //expand a Campground in the request body into a list {"title","location"}
+    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.delete('/campgrounds/:id', async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
-    res.redirect('campgrounds')
+    res.redirect('/campgrounds')
 })
 
 app.listen(3000, () => {
