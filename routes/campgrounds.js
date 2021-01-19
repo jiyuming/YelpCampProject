8c5311cb15264ab8239a3ad54697446e39ad40df
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-01-19 16:54:22 -0500

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,7 +20,7 @@ router.get('/', catchAsync(async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 }))
 
-router.get('/new', (req, res) => {
+router.get('/new', (req, res) => {  //not '/campgrounds/new' !!!!!! in 'app.js', '/campgrounds' is already provided, same for below routes
     res.render('campgrounds/new');
 })
 
