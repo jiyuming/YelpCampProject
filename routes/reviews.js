4822f37e202c61759c407678619048f048a17a52
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-01-20 17:39:03 -0500

    Update routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 const { reviewSchema } = require('../schemas');
 const Review = require('../models/review');
+const { isLoggedIn } = require('../middleware');
 
 const router = express.Router({ mergeParams: true });
 
@@ -17,7 +18,7 @@ const validateReview = (req, res, next) => {
     }
 }
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
@@ -27,7 +28,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
