4273f22a633be8689cfd0a9f461856aa59ffa5cf
Yuming Ji <50888758+jiyuming@users.noreply.github.com>
2021-01-22 20:26:16 -0500

    notes
    1. add authentication checking for certain routes
    2. save author property to a Campground
    3. the use of 'populte()' in Mongoose

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
-const { validateCampground, isLoggedIn, isAuthor } = require('../middleware');
+const { validateCampground, isLoggedIn, isAuthor } = require('../middleware');  //moved validation middlewares to one file
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -17,7 +17,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
+    campground.author = req.user._id; //save campground author
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -25,12 +25,12 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 //show a campground route
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
+    const campground = await Campground.findById(req.params.id).populate({  //'populate' is a mongoose function to connect different models; populate review's author
+        path: 'reviews', //models/campground.js 'reviews' is a property of a Campground
         populate: {
-            path: 'author'
+            path: 'author' //models/review.js 'authos' is a property of a review
         }
-    }).populate('author');
+    }).populate('author'); //populate campground author
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -39,7 +39,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }))
 
 //edit campground route
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {  //add user authentication checking
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
@@ -48,14 +48,14 @@ router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {  //add user authentication checking
     const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 //delete a campground route
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => { //add user authentication checking
     await Campground.findByIdAndDelete(req.params.id);
     req.flash('success', 'Successfully deleted campground!');
     res.redirect('/campgrounds')
